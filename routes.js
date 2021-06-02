40ccdd33b8cc393be5a4036b0fa2509e20913707
visakhts <visakhsanthosh69@gmail.com>
2021-06-02 12:50:27 +0530

    first

--- routes.js
+++ routes.js
@@ -6,9 +6,23 @@
  router.get('/',(req,res)=>{
 
 
+    let headers={
+        authority: "cdn-api.co-vin.in",
+        method: 'GET',
+        path: "/api/v2/appointment/sessions/public/calendarByPin?pincode=686653&date=02-06-2021",
+        scheme: 'https',
+        origin: "https://cowin-mern-app.herokuapp.com",
+        referer: "https://cowin-mern-app.herokuapp.com/",
+       
+        
+        }
+
+
     cron.schedule("*/10 * * * * *", async function() {
 
-        await axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=686653&date=02-06-2021')
+
+
+        await axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=686653&date=02-06-2021',{headers})
         .then((res)=>{console.log(res.data)})
         .catch((error)=>console.log(error));
    
